53efba6b5a14dc9ad696e6f14e4b7bb56f71c271
Peter Smith <peter.appliedanalyticalsciences@gmail.com>
2024-04-03 18:32:55 -0400

    renderTaskList functionality applied refactoring may be required

--- assets/js/scriptNew.js
+++ assets/js/scriptNew.js
@@ -1,7 +1,11 @@
 // empty array for tasks in local storage
-let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
 // start next id at 0 if no existing task data
 let nextId = JSON.parse(localStorage.getItem("nextId")) || 0;
+
+// establish section for appending cards after form submission
+const toDoListEL = $('#todo-cards');
 
 // task id counts +1 starting at 0 and sets value in localstorage
 function generateTaskId() {
@@ -9,30 +13,60 @@ function generateTaskId() {
     localStorage.setItem('nextId', nextId);
 }
 
-// create a task card function
+// createTaskcard function to generate task cards
 function createTaskCard(task) {
+    let taskCard = $("<div>").addClass("card drag").attr("id", task.id).css('margin', '10px');
+    let taskCardHeader = $("<h4>").addClass("card-header").text(task.title);
+    let taskCardBod = $("<div>").addClass("card-body").text(task.description);
+    let taskCardDate = $("<div>").addClass("card-footer").text(task.dateEl);
+    let taskCardId = $("<p>").addClass("card-text").text(task.id);
+    let deleteTaskBtn = $("<a>").addClass("btn btn-danger delete-task").text("Remove");
 
+    // append elements to task card body
+    taskCardBod.append(taskCardId, taskCardDate, deleteTaskBtn);
 
-    // Task card properties
-    let taskCard = $("<div>").addClass("card dragTask").attr("id", task.id).css('margin', '10px')
-    let taskTitle = $("<h4>").addClass("card-header").text(task.title);
-    let taskBody = $("<div>").addClass("card-body").text(task.description);
-    let taskDate = $("<div>").addClass("card-footer").text(task.dueDate);
-    let taskState = $("<p>").addClass("card-text").text(task.id);
-    let rmvTaskBtn = $("<a>").addClass("btn btn-danger delete-task").text("Remove Task").attr("data-id", task.id);
+    // Append header to card body
+    taskCard.append(taskCardHeader, taskCardBod);
 
-
-    // create task card
-    taskBody.append(taskState, taskDate, rmvTaskBtn);
-    taskCard.append(taskTitle, taskBody); 
+    // Return the task card
     return taskCard;
+}
 
+// generate task list with draggable cards
+function renderTaskList() {
+
+    // steps
+    // 1. retrieve tasks from local storage
+    // 2. loop through tasks and add to lane **Note- refactoring with conditional logic may be required to add tasks to lanes other than to-do"
+    // 3. make the card draggable 
+
+
+ //pull tasks from local storage
+ taskList = JSON.parse(localStorage.getItem("tasks")) || [];
+
+ // clear list to prevent duplicating tasks
+ toDoListEL.empty();
+
+ // Loop through the task list and append to 'to-do' lane
+ for (let task of taskList) {
+     let card = createTaskCard(task);
+     toDoListEL.append(card);
+ }
+
+ // add drag property to task cards
+ $(".drag").draggable({
+     zIndex: 100,
+     opacity: 0.5,
+     helper: function(event){
+         let originalcard = $(event.target).hasClass('.drag') ? $(event.target) : $(event.target).closest('.drag');
+         return originalcard.clone().css({width: originalcard.outerWidth(),});
+     }
+ });
 }
 
 
 // add new task
 function handleAddTask(event){
-
 
     // Steps
     // 1. establish form input variables
@@ -45,40 +79,32 @@ function handleAddTask(event){
 
     event.preventDefault();
 
-    // form input variables
-    let taskInputEl = $('#task-input');
-    let dateInputEl = $('#calendarSelect');
-    let taskDescriptionInputEl = $('#taskDescription');
+        // pull data from bread crumbs form
+        let titleEl = document.querySelector("#titleInput");
+        let dateEl = document.querySelector("#dateInput");
+        let descriptionEl = document.querySelector("#taskStatus");
 
     // envoke generateTaskId
     generateTaskId();
 
     // task object model
     let nuTask = {
-        title: taskInputEl.val(),
-        date: dateInputEl.val(),
-        description: taskDescriptionInputEl.val(),
+        title: titleEl.value,
+        date: dateEl.value,
+        description: descriptionEl.value,
         id: nextId,
         status: "to-do"
-
     };
 
-    // resets form
-    taskInputEl.val('');
-    dateInputEl.val('');
-    taskDescriptionInputEl.val('');
+    // reset form after bread crumb add
+    document.getElementById("form").reset();
 
-    // set local storage item for tasks
-    localStorage.setItem('tasks', JSON.stringify(tasks));
+    // push new task to local storage
+    taskList.push(nuTask);
+    localStorage.setItem('tasks', JSON.stringify(taskList));
 
-    // add task data to localstorage 
-    tasks.push(nuTask);
-
-
-    // Evoke Render task list
+    // evoke renderTaskList to populate 'To-do' lane
     renderTaskList();
-
-
 }
 
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
@@ -91,4 +117,7 @@ $(document).ready(function () {
           changeYear: true,
         });
       });
+
+    // 
+      $("#addCrumb").on("click", handleAddTask);
 });
